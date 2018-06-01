ad93627f12847d6a10a72ef123c27f19c29b2e8c
landcast <landcast@163.com>
2018-06-01 23:21:52 +0800

    debug for travis

--- integrationtests/__init__.py
+++ integrationtests/__init__.py
@@ -9,7 +9,7 @@ from config import settings
 from datetime import datetime
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
 redis_store = redis.from_url(settings.REDIS_URL)
 
@@ -23,18 +23,24 @@ class TestBase(unittest.TestCase):
     def server_check(self):
         pid_file_path = settings.PID_FILE + ".pid"
         if os.path.exists(pid_file_path):
+            logger.debug('step 10')
             with open(pid_file_path, 'r+') as pidfile:
+                logger.debug('step 11')
                 old_pid = pidfile.read()
                 # check process exist or not
                 output = subprocess.getoutput('ps -q ' + old_pid)
                 if old_pid not in output:
+                    logger.debug('step 12')
                     logger.debug('start new server')
                     os.system(
                         'nohup python run.py > /dev/null 2>&1 &')
                     time.sleep(2)
+                    logger.debug('step 13')
         else:
+            logger.debug('step 20')
             os.system('nohup python run.py > /dev/null 2>&1 &')
             time.sleep(2)
+            logger.debug('step 21')
 
     def setUp(self):
         self.server_check()
