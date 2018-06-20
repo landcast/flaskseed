993e4375c409932a58f3d970a5d0d4e2342c20e0
lxf4456 <lxf4456@163.com>
2018-06-20 15:48:00 +0800

    add student, teacher and course find

--- integrationtests/course_tests.py
+++ integrationtests/course_tests.py
@@ -14,23 +14,26 @@ server_location = 'http://127.0.0.1:5000'
 
 class CourseTest(TestBase):
 
-    def test_course(self):
+
+    def test_register(self):
         url = f'{server_location}/course/package_query'
         end = (datetime.now() + timedelta(seconds=30)).isoformat()[:-3] + 'Z'
         start = (datetime.now() + timedelta(seconds=-30)).isoformat()[:-3] + 'Z'
         json_data = "'" + json.dumps({
             "page_no": 1,
             "page_limit": 1,
+            "order_type": "1",
             "created_at_start": start,
             "created_at_end": end
         }) + "'"
         cmd = f'''
-            curl -sS -i -H {json_header} -X POST --data {json_data} {url}
-            '''
+                curl -sS -i -H {json_header} -X POST --data {json_data} {url}
+                '''
         print(cmd)
         status_code, output = subprocess.getstatusoutput(cmd)
         print(output)
         self.assertTrue('200 OK' in output, 'expect http status return 200')
+
 
 
 if __name__ == "__main__":
