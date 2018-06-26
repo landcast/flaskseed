f154a451d3aef560382cb19ff2f9527abb00d18d
lxf4456 <lxf4456@163.com>
2018-06-26 17:17:18 +0800

    find sql

--- src/blueprints/student/__init__.py
+++ src/blueprints/student/__init__.py
@@ -221,8 +221,8 @@ def my_order_sql(params):
         sql.append(' and o.id =:order_id')
 
     if 'course_name' in params.keys():
-        sql.append(" and (c.course_name like '%").append(params['course_name']).append("%'");
-        sql.append(" or c.course_name_zh like '%").append(params['course_name']).append("%')");
+        sql.append(" and (c.course_name like '%").append(params['course_name']).append("%'")\
+            .append(" or c.course_name_zh like '%").append(params['course_name']).append("%')")
 
     if 'payment_state' in params.keys():
         sql.append(' and o.payment_state = :payment_state')
