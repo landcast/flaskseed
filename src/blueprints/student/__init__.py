4fff134de2e62e59b472945572c09bda8e66860f
lxf4456 <lxf4456@163.com>
2018-06-27 10:46:51 +0800

    order Establish and refund

--- src/blueprints/student/__init__.py
+++ src/blueprints/student/__init__.py
@@ -353,17 +353,17 @@ def my_homework_sql(params):
 
     if 'homework_state' in params.keys() \
              and params['study_schedule_id']== '1':
-        sql.append(' and hm.study_schedule_id  in (elect study_schedule_id '
+        sql.append(' and hm.study_schedule_id  in (select study_schedule_id '
                    'from homework he1,study_schedule sc1 '
-                   'where homework_type = 2 and he1.`study_schedule_id` = sc1.id and sc1.`student_id` = )'
-                   + getattr(g, current_app.config['CUR_USER'])['id'])
+                   'where homework_type = 2 and he1.`study_schedule_id` = sc1.id and sc1.`student_id` = '
+                   + getattr(g, current_app.config['CUR_USER'])['id'])+')'
 
     if 'homework_state' in params.keys() \
              and params['study_schedule_id'] == '2':
         sql.append(' and hm.study_schedule_id  not in (select study_schedule_id '
                    'from homework he1,study_schedule sc1 '
-                   'where homework_type = 2 and he1.`study_schedule_id` = sc1.id and sc1.`student_id` = )'
-                   + getattr(g, current_app.config['CUR_USER'])['id'])
+                   'where homework_type = 2 and he1.`study_schedule_id` = sc1.id and sc1.`student_id` = '
+                   + getattr(g, current_app.config['CUR_USER'])['id'])+')'
 
     return ['id', 'question_name', 'homework_type', 'question_text', 'question_attachment_url',
             'answer_text', 'answer_attachment_url', 'score', 'score_remark', 'score_reason', 'created_at',
