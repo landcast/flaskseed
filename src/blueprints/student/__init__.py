9c4b64fd2dc46a8e39fe95615716d39bb660a066
lxf4456 <lxf4456@163.com>
2018-06-27 10:50:08 +0800

    order Establish and refund

--- src/blueprints/student/__init__.py
+++ src/blueprints/student/__init__.py
@@ -352,14 +352,14 @@ def my_homework_sql(params):
         sql.append(' and sc.id =:study_schedule_id')
 
     if 'homework_state' in params.keys() \
-             and params['study_schedule_id']== '1':
+             and params['homework_state']== '1':
         sql.append(' and hm.study_schedule_id  in (select study_schedule_id '
                    'from homework he1,study_schedule sc1 '
                    'where homework_type = 2 and he1.`study_schedule_id` = sc1.id and sc1.`student_id` = '
                    + getattr(g, current_app.config['CUR_USER'])['id'])+')'
 
     if 'homework_state' in params.keys() \
-             and params['study_schedule_id'] == '2':
+             and params['homework_state'] == '2':
         sql.append(' and hm.study_schedule_id  not in (select study_schedule_id '
                    'from homework he1,study_schedule sc1 '
                    'where homework_type = 2 and he1.`study_schedule_id` = sc1.id and sc1.`student_id` = '
