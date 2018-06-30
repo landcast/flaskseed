c124b7e0d10437d438d34c4e8d871d26288a1283
lxf4456 <lxf4456@163.com>
2018-06-30 15:45:09 +0800

    add Notes

--- src/blueprints/order/__init__.py
+++ src/blueprints/order/__init__.py
@@ -194,6 +194,7 @@ def establish():
         type: 'string'
     """
     order_type = request.json['order_type']
+    course_id = 0
     if 'have_course' in request.json:
         course_id = request.json['course_id']
     student_parm = request.json['student']
