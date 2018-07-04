ed56e7d6ecf481d66fe7218458facbbd73305204
litao <tao.li02@regiglobal.com>
2018-07-04 10:47:20 +0800

    fix

--- dbrecreate.sh
+++ dbrecreate.sh
@@ -2,7 +2,7 @@
 export FLASK_APP=./run.py
 sudo mysql -h 127.0.0.1  -uroot -plt7116 < ./sql/dbcreate.sql
 rm ./migrations -rf
-flask db init
-flask db migrate
-flask db upgrade
+sudo flask db init
+sudo flask db migrate
+sudo flask db upgrade
 sudo mysqldump -h 127.0.0.1  -uroot -plt7116 ustutor > ./sql/tablecreate.sql
