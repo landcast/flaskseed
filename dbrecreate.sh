a0c54568230ad243bd52889e5446a8aa52b82ec7
litao <tao.li02@regiglobal.com>
2018-07-04 09:51:19 +0800

    add sudo for non host root user run dbrecreate.sh

--- dbrecreate.sh
+++ dbrecreate.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 export FLASK_APP=./run.py
-mysql -h 127.0.0.1  -uroot -plt7116 < ./sql/dbcreate.sql
+sudo mysql -h 127.0.0.1  -uroot -plt7116 < ./sql/dbcreate.sql
 rm ./migrations -rf
 flask db init
 flask db migrate
 flask db upgrade
-mysqldump -h 127.0.0.1  -uroot -plt7116 ustutor > ./sql/tablecreate.sql
+sudo mysqldump -h 127.0.0.1  -uroot -plt7116 ustutor > ./sql/tablecreate.sql
