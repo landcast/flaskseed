3ccb9ce09e79e0d7a1bf303593e39ee58ed9edc8
lxf4456 <lxf4456@163.com>
2018-07-31 15:32:22 +0800

    course common

--- src/models/listen_models.py
+++ src/models/listen_models.py
@@ -27,5 +27,5 @@ def receive_after_update(mapper, connection, target):
     print('after_insert-1', target.__tablename__, target.id)
     current_app.logger.debug('vafter_update------------>'+target.__tablename__+'--------------'+str(target.id))
 
-    session = db.session(bind=connection)
+    session = db.session()
     listen_service.after_update(target.__tablename__, target.id, session)
