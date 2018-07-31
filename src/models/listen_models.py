8cd5f223c8f021eca75056078704a0fbfa962767
lxf4456 <lxf4456@163.com>
2018-07-31 15:31:32 +0800

    course common

--- src/models/listen_models.py
+++ src/models/listen_models.py
@@ -12,7 +12,7 @@ def receive_after_insert(mapper, connection, target):
     print('after_insert-1', target.__tablename__, target.id)
     current_app.logger.debug('after_insert---1--------->'+target.__tablename__+'--------------'+str(target.id))
 
-    session = db.session(bind=connection)
+    session = db.session()
 
     current_app.logger.debug('after_insert----2-------->')
 
