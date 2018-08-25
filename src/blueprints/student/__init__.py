e32faf83b40b71206a0003e98f0932405e89719c
lxf4456 <lxf4456@163.com>
2018-08-25 17:33:43 +0800

    interview course

--- src/blueprints/student/__init__.py
+++ src/blueprints/student/__init__.py
@@ -358,6 +358,9 @@ def my_homework():
             evaluation:
               description: '评价'
               type: 'string'
+            name:
+              description: '课节名称'
+              type: 'string'
     """
     j = request.json
     return jsonify(do_query(j, my_homework_sql))
@@ -372,7 +375,7 @@ def my_homework_sql(params):
     current_app.logger.debug(params)
     sql = ['''
     select hm.id,question_name,homework_type,question_text,question_attachment_url,answer_text,answer_attachment_url,score,score_remark,score_reason,hm.created_at,concat(t.first_name,' ',t.middle_name,' ',t.last_name)  as teacher_name,c.course_name_zh as course_name,
-    t.avatar as teacher_avatar,sc.teacher_evaluation,cs.name,sc.student_evaluation,hm.evaluation
+    t.avatar as teacher_avatar,sc.teacher_evaluation,cs.name,sc.student_evaluation,hm.evaluation,sc.name
     from homework hm,study_schedule sc,course c,teacher t,course_schedule cs
     where 
     hm.study_schedule_id = sc.id and cs.course_id = c.id and c.`primary_teacher_id` = t.id and sc.course_schedule_id = cs.id and hm.homework_type = 1
@@ -403,7 +406,7 @@ def my_homework_sql(params):
 
     return ['id', 'question_name', 'homework_type', 'question_text', 'question_attachment_url',
             'answer_text', 'answer_attachment_url', 'score', 'score_remark', 'score_reason', 'created_at',
-            'teacher_name','course_name','teacher_avatar','teacher_evaluation','name','student_evaluation','evaluation'], ''.join(sql)
+            'teacher_name','course_name','teacher_avatar','teacher_evaluation','name','student_evaluation','evaluation','name'], ''.join(sql)
 
 
 @student.route('/report_card', methods=['POST'])
