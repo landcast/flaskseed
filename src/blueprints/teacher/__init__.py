39d7cc78a9a3f05bfa57193aae4089b8599bc23c
lxf4456 <lxf4456@163.com>
2018-09-08 20:34:49 +0800

    interview course

--- src/blueprints/teacher/__init__.py
+++ src/blueprints/teacher/__init__.py
@@ -1646,24 +1646,24 @@ def accept_interview():
     interview_at_end = request.json['interview_at_end']
 
     with session_scope(db) as session:
-
+        current_app.logger.debug('accept_interview---------------->1')
         interview = session.query(Interview).filter_by(id=interview_id).one_or_none()
-
+        current_app.logger.debug('accept_interview---------------->2')
         if interview is None :
             return jsonify({
                 "error": "not found interview: {0}".format(
                     interview_id)
             }), 500
 
-
         setattr(interview,'start',interview_at_start.replace('T', ' ').replace('Z', ''))
         setattr(interview,'end',interview_at_end.replace('T', ' ').replace('Z', ''))
         setattr(interview,'state',2)
         setattr(interview,'interviewer_id',getattr(g, current_app.config['CUR_USER'])['id'])
         setattr(interview,'updated_by',getattr(g, current_app.config['CUR_USER'])['username'])
+        current_app.logger.debug('accept_interview---------------->3')
         session.add(interview)
         session.flush()
-
+        current_app.logger.debug('accept_interview---------------->4')
         course =Course( course_type= 1,
                         class_type= 3,
                         classes_number = 1,
