316840a82d0966d6e39aa9ec6c4de27b17996c5f
Tom Li <landcast@163.com>
2018-10-08 21:40:15 +0800

    fix path

--- dbrecreate_prd.sh
+++ dbrecreate_prd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export EXTERNALCFG=./config/settings-prd.py
+export EXTERNALCFG=../config/settings-prd.py
 export FLASK_APP=./run.py
 sudo mysql -h rm-2ze6kzf5ig80613f7vo.mysql.rds.aliyuncs.com  -uustutor -pUs_Tutor < ./sql/dbcreate.sql
 rm ./migrations -rf
