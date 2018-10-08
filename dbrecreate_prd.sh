3b055544b155741901ddde3c3a9122040ec65b57
lxf4456 <lxf4456@163.com>
2018-10-08 21:24:49 +0800

    interview course

---
+++ dbrecreate_prd.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+export FLASK_APP=./run.py
+sudo mysql -h rm-2ze6kzf5ig80613f7vo.mysql.rds.aliyuncs.com  -uustutor -pUs_Tutor < ./sql/dbcreate.sql
+rm ./migrations -rf
+flask db init
+flask db migrate
+flask db upgrade
+sudo mysqldump -h rm-2ze6kzf5ig80613f7vo.mysql.rds.aliyuncs.com  -uustutor -pUs_Tutor  ustutor > ./sql/tablecreate.sql
