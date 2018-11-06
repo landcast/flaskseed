da8f56c0907d234377738c8f83382dbe78ebd3a3
lxf4456 <lxf4456@163.com>
2018-11-06 19:47:42 +0800

    interview course

--- src/blueprints/student/__init__.py
+++ src/blueprints/student/__init__.py
@@ -1243,7 +1243,7 @@ def start_course_sql(params):
     sql = ['''
     select c.course_name,ss.name,concat(IFNULL(t.first_name,''),' ',IFNULL(t.middle_name,''),' ',IFNULL(t.last_name,'')) as teacher_name,ss.actual_start start,ss.actual_end end from 
     study_schedule ss,course_schedule cs,course c,teacher t
-    where ss.`course_schedule_id` = cs.id and cs.course_id = c.id and c.primary_teacher_id = t.id
+    where ss.`course_schedule_id` = cs.id and cs.course_id = c.id and c.primary_teacher_id = t.id  and ss.actual_start > now()
      and ss .`delete_flag` = 'IN_FORCE' and cs .`delete_flag` = 'IN_FORCE'  and c .`delete_flag` = 'IN_FORCE' and t .`delete_flag` = 'IN_FORCE'
          ''']
 
